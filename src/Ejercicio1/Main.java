95212763c0894b79c2ea37017407f7bdf368ee0d
Fabrizzio2k1 <fabrizzio.federico@davinci.edu.ar>
2024-10-27 15:28:17 -0300

    Menu de Vehiculo

--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -8,15 +8,23 @@ public class Main {
 		// TODO Auto-generated method stub
 		Duenio d1 = new Duenio();
 		Auto a1 = new Auto(d1);
+		Moto m1 = new Moto(d1);
 		String opciones [] = {"Ingrese Vehiculo","Salir"};
+		String vehiculo [] = {"Moto","Auto"};
 		int elegido;
 		do {
 			elegido = JOptionPane.showOptionDialog(null, "Elija una opcion", "Taller Tano", 0, 0, null, opciones, opciones[0]);
 			switch (elegido) {
 			case 0:
-				a1.asignarDuenio(d1);
-				break;
-
+				elegido = JOptionPane.showOptionDialog(null, "Elija una opcion", "Taller Tano", 0, 0, null, vehiculo, vehiculo[0]);
+				switch (elegido) {
+				case 0:
+					a1.asignarDuenio(d1);
+					break;
+				case 1:
+					m1.asignarDuenio(d1);
+					break;
+				}
 			default:
 				break;
 			}
